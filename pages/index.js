24faa6c769da3c805206da99f1dbc7e82250dfd0
Febriqgal Purnama <febriqgalp@gmail.com>
2022-11-12 22:15:31 +0700

    dddddd

--- pages/index.js
+++ pages/index.js
@@ -27,13 +27,13 @@ import linux from "../public/linux.svg";
 
 export default function Home() {
   return (
-    <div className={`${styles.bgg}`}>
+    <>
       <Head>
         <title>Febriqgal Purnama</title>
         <meta name="description" content="Febriqgal" />
         <link rel="icon" href="/icon.png" />
       </Head>
-      <main className={`${styles.main}`}>
+      <main className={`${styles.main} ${styles.bgg}`}>
         <div
           className={`bg-slate-50 w-[400px] text-slate-900 shadow-lg py-5 px-10 flex flex-col rounded-lg`}
         >
@@ -101,7 +101,7 @@ export default function Home() {
               <Image title={"Windows"} src={Windows} alt={"Windows"} />
               <Image title={"Linux"} src={linux} alt={"Linux"} />
             </div>
-            <Link target={"#"} href={"https://www.instagram.com/febproject_/"}>
+            <Link href={"/project"}>
               <h1 className="mx-auto w-[100px] mt-5 bg-black text-slate-50 text-center rounded-lg">
                 Project
               </h1>
@@ -110,7 +110,7 @@ export default function Home() {
           <div className="flex justify-center gap-4 pt-4">
             <Link target={"#"} href={"https://github.com/febriqgal"}>
               <Image
-                className="h-5"
+                className="h-auto"
                 title={"Github"}
                 src={Github}
                 alt={"Github"}
@@ -118,7 +118,7 @@ export default function Home() {
             </Link>
             <Link target={"#"} href={"https://www.instagram.com/febriqgal_/"}>
               <Image
-                className="h-5"
+                className="h-auto"
                 title={"Instagram"}
                 src={Instagram}
                 alt={"Instagram"}
@@ -126,7 +126,7 @@ export default function Home() {
             </Link>
             <Link target={"#"} href={"https://twitter.com/febriqgal_"}>
               <Image
-                className="h-5"
+                className="h-auto"
                 title={"Twitter"}
                 src={Twitter}
                 alt={"Twitter"}
@@ -134,15 +134,15 @@ export default function Home() {
             </Link>
             <Link target={"#"} href={"https://www.linkedin.com/in/febriqgal/"}>
               <Image
-                className="h-5"
+                className="h-auto"
                 title={"Linkedin"}
                 src={Linkedin}
                 alt={"Linkedin"}
               />
             </Link>
-            <Link to target={"#"} href={"mailto:febriqgalp@gmail.com"}>
+            <Link target={"#"} href={"mailto:febriqgalp@gmail.com"}>
               <Image
-                className="h-5"
+                className="h-auto"
                 title={"Gmail"}
                 src={Gmail}
                 alt={"Gmail"}
@@ -154,6 +154,6 @@ export default function Home() {
           <h1>© 2022, Febriqgal.</h1>
         </footer>
       </main>
-    </div>
+    </>
   );
 }
